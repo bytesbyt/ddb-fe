87d437c96634e212982804da7e9f084f7c0dea1b
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-08-03 16:15:39 +0100

    add proxy setting

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 const LOCAL_BACKEND = `${process.env.REACT_APP_LOCAL_BACKEND}/api`;
 const PROD_BACKEND = `${process.env.REACT_APP_PROD_BACKEND}/api`;
-// const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
+const BACKEND_PROXY = `${process.env.REACT_APP_BACKEND_PROXY}/api`;
 // console.log("proxy", BACKEND_PROXY);
 
 // Use production backend if available, otherwise use local
 const baseURL = PROD_BACKEND || LOCAL_BACKEND;
 
 const api = axios.create({
-  baseURL: baseURL,
+  baseURL: BACKEND_PROXY,
   headers: {
     "Content-Type": "application/json",
   },
