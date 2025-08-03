2e66600303aa7b89bf740966818441b79532d72e
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-08-03 19:47:33 +0100

    authenticated users will be redirected to the home page if they try to access /login via URL

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loginWithToken } from "./features/user/userSlice";
 
 import "./common/style/common.style.css";
 import AppLayout from "./Layout/AppLayout";
 import AppRouter from "./routes/AppRouter";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+    if (token) {
+      dispatch(loginWithToken());
+    }
+  }, [dispatch]);
+
   return (
     <div>
       <AppLayout>
