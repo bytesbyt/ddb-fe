4fc5af80a30c9ecdb7974b9d58759b00dcfd2df4
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-08-03 17:15:07 +0100

    update erro display

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -5,7 +5,16 @@ import api from "../../utils/api";
 
 export const loginWithEmail = createAsyncThunk(
   "user/loginWithEmail",
-  async ({ email, password }, { rejectWithValue }) => {}
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const response = await api.post("/auth/login", {email,password});
+
+      return response.data;
+
+    }catch (error){
+      return rejectWithValue(error.response?.data?.message || error.response?.data?.error || error.message);
+    }
+  }
 );
 
 export const loginWithGoogle = createAsyncThunk(
@@ -78,7 +87,19 @@ const userSlice = createSlice({
     })
     .addCase(registerUser.rejected, (state, action) => {
       state.registrationError = action.payload;
-    });
+    })
+    .addCase(loginWithEmail.pending, (state) => {
+      state.loading = true;
+    })
+    .addCase(loginWithEmail.fulfilled, (state, action) => {
+      state.loading = false;
+      state.user = action.payload.user;
+      state.loginError = null;
+    })
+    .addCase(loginWithEmail.rejected, (state, action) => {
+      state.loading = false;
+      state.loginError = action.payload;
+    })
   },
 });
 export const { clearErrors } = userSlice.actions;
