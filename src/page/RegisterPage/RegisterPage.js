df5f78e3b594517a35af5dcea7a562c1364d27a6
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-08-04 04:34:24 +0100

    update registerpage

--- src/page/RegisterPage/RegisterPage.js
+++ src/page/RegisterPage/RegisterPage.js
@@ -31,7 +31,7 @@ const RegisterPage = () => {
   useEffect(() => {
     // Clear errors 
     dispatch(clearErrors());
-  }, []);
+  }, [dispatch]);
 
   const register = (event) => {
     event.preventDefault();
